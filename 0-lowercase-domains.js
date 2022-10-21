934ec5c4b6fe705830af0428eee8bb95bbe73e2e
Maria <mtens17@gmail.com>
2022-10-21 12:58:06 +0100

    Add pseudocode

---
+++ 0-lowercase-domains.js
@@ -0,0 +1,15 @@
+// Write a function that takes an array of strings representing domain names, in mixed case, and returns an array of the domain names in all-lowercase.
+
+/* pseudocode
+function: lowerArray
+params:
+    - array
+return: array
+=====
+let resultArray = []
+for each item of array
+    change item to lower case 
+    push item to resultArray
+end for 
+return resultArray
+*/
