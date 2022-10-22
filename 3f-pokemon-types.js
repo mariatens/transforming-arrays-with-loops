d168de795b36541d2d97f20e588765d5d9b0469a
Maria <mtens17@gmail.com>
2022-10-22 11:55:21 +0100

    Add for loop and map for 3f

--- 3f-pokemon-types.js
+++ 3f-pokemon-types.js
@@ -1,13 +1,33 @@
-// Write a function that takes an array of objects representing Pokémon, with string properties name and type, and returns a descriptive sentence about them and their type: "<name> is a <type in lowercase> type Pokémon.".
+// Write a function that takes an array of objects representing Pokémon, 
+// with string properties name and type, and returns a descriptive sentence about them and their type: "<name> is a <type in lowercase> type Pokémon.".
 
 
 function descPokemon(arr){
+        let newArr = []
         for (obj of arr){
-                let sent = `${obj.name} is a ${obj.type} Pokémon`
+                let sent = obj.name + " is a " + obj.type.toLowerCase() + " Pokémon."
+                newArr.push(sent)
         }
-        return sent
+        return newArr
 }
+
 
 // test case
 console.log(descPokemon([{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]),
         ["Bulbasaur is a grass type Pokémon.", "Charmander is a fire type Pokémon.", "Squirtle is a water type Pokémon."])
+
+// using map
+
+//transforming function 
+function text(obj){
+        return obj.name + " is a " + obj.type.toLowerCase() + " Pokémon."
+}
+
+//passing map 
+function objToText(objArr){
+        return objArr.map(text)
+}
+
+//test cases
+console.log(objToText([{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]),
+     ["Bulbasaur is a grass type Pokémon.", "Charmander is a fire type Pokémon.", "Squirtle is a water type Pokémon."])
