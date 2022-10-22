8bef77e3a9b0a19b064a3214fa7f08801281f901
Maria <mtens17@gmail.com>
2022-10-22 10:32:08 +0100

    Try different way of doing 3d and try map

--- 3d-only-initials.js
+++ 3d-only-initials.js
@@ -1,4 +1,5 @@
-//Write a function that takes an array of names (always <firstname> <surname> format) and returns an array of initials for each person.
+//Write a function that takes an array of names (always <firstname> <surname> format) 
+//and returns an array of initials for each person.
 
 function initial(nameArr){
     let initialArr = []
@@ -15,3 +16,27 @@ function initial(nameArr){
 
 //test case
 console.log(initial(["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"]), ["A.K.", "L.H.", "Y.M."])
+
+// using map 
+//transforming function 
+
+function initials(name){
+    let newStr = ""
+    for (let letter of name){
+        if (letter == letter.toUpperCase()){
+            let newLet = letter + "."
+            newStr.concat(newLet)
+            console.log(newLet)
+        }
+    }
+    return newStr
+}
+
+console.log(initials("Ash Ketchum"))
+
+//passing map 
+function initialArr (arr){
+    return arr.map(initials)
+}
+
+console.log(initialArr(["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"]), ["A.K.", "L.H.", "Y.M."])
