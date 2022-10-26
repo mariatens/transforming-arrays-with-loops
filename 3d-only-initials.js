66d04362b52b72084cc4a51af6baa56bf19d1c72
Maria <mtens17@gmail.com>
2022-10-26 12:42:15 +0100

    Fix 3d code. Spaces were considered upper case weirdly

--- 3d-only-initials.js
+++ 3d-only-initials.js
@@ -1,35 +1,33 @@
 //Write a function that takes an array of names (always <firstname> <surname> format) 
 //and returns an array of initials for each person.
 
-function initial(nameArr){
-    let initialArr = []
-    for (str of nameArr){
-        for (letter of str){
-            if (letter == letter.toUpperCase()){
-                initialArr.push(`${letter}.`)
-            }
-        }
-    }
-    return initialArr.join()
-}
+// function initial(nameArr){
+//     let initialArr = []
+//     for (str of nameArr){
+//         for (letter of str){
+//             if (letter == letter.toUpperCase()){
+//                 initialArr.push(letter)
+//             }
+//         }
+//     }
+//     return initialArr.join(' ')
+// }
 
-
-//test case
-console.log(initial(["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"]), ["A.K.", "L.H.", "Y.M."])
+// //test case
+// console.log(initial(["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"]), ["A.K.", "L.H.", "Y.M."])
 
 // using map 
 //transforming function 
 
 function initials(name){
-    let newStr = ""
+    let newStr = []
     for (let letter of name){
-        if (letter == letter.toUpperCase()){
-            let newLet = letter + "."
-            newStr.concat(newLet)
-            console.log(newLet)
+        if (letter !== letter.toLowerCase()){
+            newStr.push(`${letter}.`)
+            console.log(newStr)
         }
     }
-    return newStr
+    return newStr.join('')
 }
 
 console.log(initials("Ash Ketchum"))
